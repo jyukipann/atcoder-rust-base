e68d0328abf1411792514bb518db96c6afc57ca2
JUKI <jyukipann@yahoo.co.jp>
2023-05-04 17:06:13 +0900

    Update sample_inputs.rs

--- tests/sample_inputs.rs
+++ tests/sample_inputs.rs
@@ -5,14 +5,14 @@ const BIN: &'static str = "./main";
 #[test]
 fn sample1() {
     let input = r#""#;
-    let output = r#""#;
+    let  ans = "";
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
         .output_with_stdin(input)
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), output);
+    assert_eq!(output.stdout_str(), ans);
     assert!(output.stderr_str().is_empty());
 }
 
